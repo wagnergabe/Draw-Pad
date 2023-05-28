7a3b7e6045858b58f4c2404027440430d5f59d67
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-05-27 21:05:01 -0500

    updated size change/and internet went out...

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
 
-let size = 20;
+let size = 10;
 let isPressed = false
 let color = 'black'
 let x
@@ -49,3 +54,20 @@ function drawLine(x1, y1, x2, y2) {
     ctx.stroke()
 }
 
+function updateSizeOnScreen() {
+    sizeEl.innerText = size;
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5;
+
+    if(size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => {
+    color = e.target.value
+})
